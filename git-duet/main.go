80faf693ccd4f9977949d24d489e850a0b7506aa
Rafe Colton <r.colton@modcloth.com>
2014-06-09 10:44:44 -0700

    Go Fmt'ing and adding basic specs for ginkgo
    adding trivial ginkgo specs just so we have something to run

--- git-duet/main.go
+++ git-duet/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("running git-duet")
+	fmt.Println("running git-duet")
 }
---
+++ version/version_test.go
@@ -0,0 +1,39 @@
+package version_test
+
+import (
+	. "github.com/modcloth/go-git-duet/version"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func TestBuilder(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Version Specs")
+}
+
+var _ = Describe("Version", func() {
+	var (
+		subject *Version
+	)
+
+	BeforeEach(func() {
+		BranchString = "bogus-branch"
+		RevString = "1234567890"
+		VersionString = "12345-test"
+
+		subject = NewVersion()
+	})
+
+	It("prints the correct branch", func() {
+		Expect(subject.Branch).To(Equal("bogus-branch"))
+	})
+
+	It("prints the correct rev", func() {
+		Expect(subject.Rev).To(Equal("1234567890"))
+	})
+
+	It("prints the correct version", func() {
+		Expect(subject.Version).To(Equal("12345-test"))
+	})
+})
